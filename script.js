f0df65a0100354f7cc3aaeb9efd08d77082eb9ca
Anastasia Legert <legertlab@gmail.com>
2019-09-26 13:57:17 +0200

    Made restart of game

--- script.js
+++ script.js
@@ -4,13 +4,40 @@ const middleButton = document.getElementById("middle");
 const hardButton = document.getElementById("hard");
 const startButton = document.getElementById("start");
 const menuScreen = document.getElementById("menuScreen");
-const baseField = document.querySelector(".container-for-cards");
+const mainContainer = document.querySelector(".container-background");
 const allButtonList = document.querySelectorAll(".menu__item");
+// создаем внутренние элементы карты
+//const baseField = document.querySelector(".container-for-cards");
+const baseField = document.createElement("div");
+baseField.classList.add(".container-for-cards");
 
 const card = (level) => {
-    const newCard = document.createElement('div');
+    const newCard = document.createElement("div");
+    const flipCardInner = document.createElement("div");
+    const flipCardFront = document.createElement("div");
+    const flipCardBack = document.createElement("div");
+    const imgValue = document.createElement("img");
+    const imgFront = document.createElement("img");
     newCard.classList.add("card");
+    flipCardInner.classList.add("flip-card-inner");
+    flipCardFront.classList.add("flip-card-front");
+    flipCardBack.classList.add("flip-card-back");
+    imgValue.setAttribute("src", "card_gameover.png");
+    imgFront.setAttribute("src", "card_back.png");
     baseField.appendChild(newCard);
+    newCard.appendChild(flipCardInner);
+    flipCardInner.appendChild(flipCardFront);
+    flipCardInner.appendChild(flipCardBack);
+    flipCardFront.appendChild(imgFront);
+    flipCardBack.appendChild(imgValue);
+    flipCardInner.addEventListener("click", () => {
+        flipCardInner.classList.toggle("transform");
+        flipCardInner.addEventListener("click", () => {
+            mainContainer.appendChild(menuScreen);
+            baseField.innerHTML = '';
+            baseField.remove()
+            });
+        });
     //функция, генерирующая обратную сторону карты Math.random
 }
 
@@ -30,8 +57,10 @@ allButtonList.forEach((item) => item.addEventListener("click", chooseLevel));
 function startGame () {
     let currentLevel = document.querySelector(".checked").getAttribute("id");
     menuScreen.remove();
+    mainContainer.appendChild(baseField);
 
     function createField (level) {
+
         baseField.classList.add("centered")
         switch (level) {
             case "simple":
